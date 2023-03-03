1239f12c13c4379245c26af3c7d65c717e920ac0
Arthur Bispo <arthurbispo94@gmail.com>
2023-03-03 18:10:01 -0300

    add feature: When creating a beach

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import { ForecastControler } from './controllers/forecast';
 import { Application } from 'express';
 import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -21,7 +22,8 @@ export class SetupServer extends Server {
   }
   private setupControllers(): void {
     const forecastController = new ForecastControler();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
   private async databaseSetup(): Promise<void> {
