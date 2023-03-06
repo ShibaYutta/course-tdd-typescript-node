a847df6291a4f72b4d166063d3c63a768c473ade
Arthur Bispo <arthurbispo94@gmail.com>
2023-03-06 16:11:22 -0300

    add config: server start

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -17,6 +17,7 @@ export class ForecastControler {
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
     } catch (error: any) {
+      console.error(error);
       res.status(500).send({ error: 'Something went wrong' });
     }
   }
