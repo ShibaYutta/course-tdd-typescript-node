fc1df4240a2fa2ea484b22b441d91bc5d0a44e19
Arthur Bispo <arthurbispo94@gmail.com>
2023-03-08 16:05:38 -0300

    change feature: user password now is encryped

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import AuthService from '@src/services/auth';
 
 export interface User {
   _id?: string;
@@ -42,6 +43,18 @@ schema.path('email').validate(
   'already exists in the database.',
   CUSTOM_VALIDATION.DUPLICATED
 );
+
+schema.pre('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (error) {
+    console.error(`Error hashing the password for the user`);
+  }
+});
 
 export const User: mongoose.Model<UserModel> = mongoose.model<UserModel>(
   'User',
