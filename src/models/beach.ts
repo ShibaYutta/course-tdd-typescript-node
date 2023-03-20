1f414a6afe8bc1d496640a76bcc884e1bf44e311
Arthur Bispo <ARTHURBISPO94@GMAIL.COM>
2023-03-20 13:11:06 -0300

    feature add: alot...

--- src/models/beach.ts
+++ src/models/beach.ts
@@ -13,6 +13,7 @@ export interface Beach {
   position: BeachPosition;
   lat: number;
   lng: number;
+  user: string;
 }
 
 const schema = new mongoose.Schema(
@@ -21,11 +22,11 @@ const schema = new mongoose.Schema(
     lng: { type: Number, required: true },
     name: { type: String, required: true },
     position: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   },
   {
     toJSON: {
       transform: (_, ret): void => {
-        ret.id = ret._id;
         delete ret._id;
         delete ret.__v;
       },
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,9 +1,20 @@
 import { Beach } from '@src/models/beach';
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 jest.setTimeout(10000);
 describe('Beaches functional test', () => {
-  beforeAll(async () => {
+  const defaultUser = {
+    name: 'John Doe',
+    email: 'john2@mail.com',
+    password: '1234',
+  };
+  let token: string;
+  beforeEach(async () => {
     await Beach.deleteMany({});
+    await User.deleteMany({});
+    const user = await new User(defaultUser).save();
+    token = AuthService.generateToken(user.toJSON());
   });
   describe('When creating a beach', () => {
     it('should create a beach with sucess', async () => {
@@ -14,7 +25,10 @@ describe('Beaches functional test', () => {
         position: 'E',
       };
 
-      const response = await global.testRequest.post('/beaches').send(newBeach);
+      const response = await global.testRequest
+        .post('/beaches')
+        .set({ 'x-access-token': token })
+        .send(newBeach);
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
@@ -25,7 +39,10 @@ describe('Beaches functional test', () => {
         name: 'Manly',
         position: 'E',
       };
-      const response = await global.testRequest.post('/beaches').send(newBeach);
+      const response = await global.testRequest
+        .post('/beaches')
+        .set({ 'x-access-token': token })
+        .send(newBeach);
 
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
